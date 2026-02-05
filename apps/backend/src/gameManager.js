233a919eb83d1a94d10b5f859c73f34d7d27776c
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-06 00:45:48 +0530

    Added game manager

--- apps/backend/src/gameManager.js
+++ apps/backend/src/gameManager.js
@@ -24,23 +24,23 @@ export class GameManager {
     addHandler(socket){
         socket.on("message", (data) => {
             const message = JSON.parse(data.toString());
-        })
 
-        if(message.type === INIT_GAME){
+            if(message.type === INIT_GAME){
             if(this.pendingUsers){
                 const game = new Game(this.pendingUsers, socket)
                 this.games.push(game);
                 this.pendingUsers = null;
             } else {
-                this.pendingUsers = socket;
+                    this.pendingUsers = socket;
+                }
             }
-        }
 
-        if(message.type === MOVE){
-            const game = this.games.find(g => g.player1 === socket || game.player2 === socket);
-            if(game){
-                game.makeMove(socket, message.payload);
+            if(message.type === MOVE){
+                const game = this.games.find(g => g.player1 === socket || game.player2 === socket);
+                if(game){
+                    game.makeMove(socket, message.payload);
+                }
             }
-        }
+        })
     }
 };
