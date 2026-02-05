1e4e9030d7007858d823bf86d7f191627d5c050c
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-05 23:53:25 +0530

    Added game

--- apps/backend/src/game.js
+++ apps/backend/src/game.js
@@ -1,9 +1,10 @@
+import { Chess } from 'chess.js';
 export class Game{
 
     constructor(player1, player2, board, moves, startTime){
         this.player1 = player1;
         this.player2 = player2;
-        this.board = board;
+        this.board = new Chess();
         this.moves = [];
         this.startTime = new Date()
     }
