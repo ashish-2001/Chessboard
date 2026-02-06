1962dc9d7244df1e7005c6fe5227db88e1d59cae
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-06 23:29:53 +0530

    Added Landing page

--- apps/web/src/screen/Landing.jsx
+++ apps/web/src/screen/Landing.jsx
@@ -1,10 +1,24 @@
+import Chessboard from "../assets/Chessboard.jpeg";
+
 function Landing(){
     return (
-        <>
-            <div>
-                This is a Landing page
+        <div>
+            <div className="mt-2">
+                <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+                    <div className="flex justify-center">
+                        <img src={Chessboard} className="max-w-96"/>
+                    </div>
+                    <div>
+                        <h1 className="text-4xl font-bold">Play chess online on the Site!</h1>
+                        <div className="mt-4">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Play Online
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </>
+        </div>
     )
 };
 
