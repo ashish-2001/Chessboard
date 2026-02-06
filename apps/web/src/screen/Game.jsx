a455453cf5e86ca788ddfaf935a1c16f9d63e6b2
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-07 00:22:41 +0530

    Added Game

--- apps/web/src/screen/Game.jsx
+++ apps/web/src/screen/Game.jsx
@@ -1,9 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/Button"
 import { Chessboard } from "../components/Chessboard";
+import { useSocket } from "../hooks/useSocket";
+import { useEffect } from "react";
+import { GAME_OVER, INIT_GAME, MOVE } from "../../../backend/src/messages";
+
+
 
 function Game(){
-    const navigate = useNavigate();
+    const socket = useSocket();
+
+    useEffect(() => {
+        if(!socket){
+            return;
+        }
+
+        socket.onmessage = (e) => {
+            const message = JSON.parse(e.data);
+            console.log(message);
+            switch(message.type){
+                case INIT_GAME:
+                    console.log("Game Initiator");
+                    break;
+
+                case MOVE:
+                    console.log("Move made");
+                    break;
+                case GAME_OVER:
+                    console.log("Game over");
+                    break;
+            }
+        }
+    }, [socket]);
+    
     return (
         <div className="flex justify-center">
             <div className="pt-8 max-w-3xl">
