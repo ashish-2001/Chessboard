54ed64328be70033eb9db129a016ae96bfe7fc5c
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-06 23:46:09 +0530

    Added Landing Page

--- apps/web/src/screen/Landing.jsx
+++ apps/web/src/screen/Landing.jsx
@@ -1,17 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import Chessboard from "../assets/Chessboard.jpeg";
 
 function Landing(){
+
+    const navigate = useNavigate();
     return (
-        <div>
-            <div className="mt-2">
+        <div className="flex justify-content">
+            <div className="pt-8 max-w-3xl">
                 <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
                     <div className="flex justify-center">
                         <img src={Chessboard} className="max-w-96"/>
                     </div>
-                    <div>
-                        <h1 className="text-4xl font-bold">Play chess online on the Site!</h1>
-                        <div className="mt-4">
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <div className="pt-16">
+                        <div>
+                            <h1 className="text-4xl font-bold text-white">Play chess online on the Site!</h1>
+                        </div>
+                        <div className="mt-4 flex justify-center">
+                            <button onClick={() => { navigate("/game")}} className="px-8 py-4 text-2xl bg-green-500 hover:bg-green-700 text-white font-bold rounded">
                                 Play Online
                             </button>
                         </div>
