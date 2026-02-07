e65962bba46e3b8e6ed22cf4c78514817a4ef907
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-07 23:10:54 +0530

    Added chessboard

--- apps/web/src/components/Chessboard.jsx
+++ apps/web/src/components/Chessboard.jsx
@@ -1,11 +1,13 @@
 function Chessboard({ board }){
 
+    if(!board) return null;
+
     return (
         <div>
             {board.map((row, i) => {
                 return <div key={i} className="flex">
                     {row.map((square, j) => {
-                        return <div key={j} className={`w-8 h-8 ${(i+j) % 2 ? 'bg-green-200' : "bg-green-300"}`}>
+                        return <div key={j} className={`w-12 h-12 flex items-center justify-center text-xl ${(i+j) % 2 ? 'bg-green-300' : "bg-green-200"}`}>
                             {square ? square.type : ""}
                         </div>
                     })}
