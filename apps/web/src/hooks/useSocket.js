28d4b08d333a4e8337a6e1f8077f0f11d2dde6aa
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-07 22:57:57 +0530

    Added usesocket hook

--- apps/web/src/hooks/useSocket.js
+++ apps/web/src/hooks/useSocket.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 
-const WS_URL = "ws://localhost:8080"
+const WS_URL = "ws://localhost:8080";
+
 const useSocket = () => {
     const [socket, setSocket] = useState(null);
 
