6484bfc5dc1ad18c091a9ea678374656383eb432
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-12 22:33:07 +0530

    Added app

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-express
+import express from "express";
+import { socket } from "socket.io";
+import http from "http";
+import { Chess } from "chess.js";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "w";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(dir_name, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
