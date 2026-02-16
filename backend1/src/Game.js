002ff10db4bb130e9811a1e4383e7fc79c897d5f
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-16 23:17:43 +0530

    Added Game

---
+++ backend1/src/Game.js
@@ -0,0 +1,9 @@
+class Game {
+    constructor(player1, player2){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = "";
+        this.moves = [];
+        this.startTime = new Date();
+    }
+}
