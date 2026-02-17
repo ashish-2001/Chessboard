4bbca2753df64972edbcfe930868d807547dbd30
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 16:55:46 +0530

    Added game

--- frontend/src/screens/Game.jsx
+++ frontend/src/screens/Game.jsx
@@ -48,11 +48,11 @@ function Game(){
     return (
         <div  className="flex justify-center">
             <div className="pt-8 max-w-5xl w-full">
-                <div className="grid grid-cols-6 gap-4 w-full">
-                    <div className="grid-cols-span-4  w-full flex justify-center">
+                <div className="flex justify-center gap-4 w-full">
+                    <div className="grid-cols-span-4 w-full flex justify-center">
                         <Chessboard board={board}/>
                     </div>
-                    <div className="col-span-2 bg-green-200 w-full flex justify-center">
+                    <div className="col-span-2 bg-green-200 w-1/2 flex justify-center">
                         <div className="pt-8">
                             <Button onClick={() => {
                                 socket.send(JSON.stringify({
