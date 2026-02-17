8bfe82b20f2002531949e942ec126bc12f123d1f
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 13:04:32 +0530

    Added index

--- backend1/src/index.js
+++ backend1/src/index.js
@@ -5,7 +5,6 @@ const wss = new WebSocketServer({ port: 8080 });
 const gameManager = new GameManager();
 
 wss.on("connection", function connection(ws){
-    
     gameManager.addUser(ws);
     ws.on("disconnect", () => gameManager.removeUser(ws));
 });
