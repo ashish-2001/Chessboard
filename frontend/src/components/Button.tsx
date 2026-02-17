a30d1b9d8250dbfb1c4524754e8e4aadce73fc09
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 15:37:14 +0530

    Added Button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,12 +1,15 @@
-function Button(){
+import { useNavigate } from "react-router-dom"
 
+function Button({ children, onClick}){
+
+    const navigate = useNavigate();
     return(
-        <div>
-
-        </div>
+        <button onClick={onClick} className="px-8 py-4 text-2xl bg-green-500 hover:bg-green-700 text-white font-bold rounded">
+            {children}
+        </button>
     )
 }
 
 export {
     Button
-}
+};
