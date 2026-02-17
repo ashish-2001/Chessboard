4ffbc0af4e8c500776b753833382cdb2d63cb2ff
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 16:46:59 +0530

    Added Chessboard

--- frontend/src/components/Chessboard.jsx
+++ frontend/src/components/Chessboard.jsx
@@ -4,8 +4,12 @@ function Chessboard({ board }){
             {board.map((row, i) => {
                 return <div key={i} className="flex ">
                     {row.map((square, j) => {
-                        return <div key={j} className={`w-16 h-16 ${(i+j)%2 === 0 ? 'bg-green-500' : 'bg-green-300'}`}> 
-                            {square ? square.type : ""}
+                        return <div key={j} className={`w-16 h-16 ${(i+j)%2 === 0 ? 'bg-green-500' : 'bg-white'}`}> 
+                            <div className="w-full justify-center flex h-full">
+                                <div className="h-full justify-center flex flex-col">
+                                    {square ? square.type : ""}
+                                </div>
+                            </div>
                         </div>
                     })}
                 </div>
