f1c012d78d54de8763533b15a9346f031c970495
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 18:32:18 +0530

    Added game manager

--- backend1/src/GameManager.js
+++ backend1/src/GameManager.js
@@ -36,7 +36,7 @@ class GameManager {
                 const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
                 if(game){
                     console.log("Inside make move");
-                    game.makeMove(socket, message.payload);
+                    game.makeMove(socket, message.move);
                 }
             }
         })
