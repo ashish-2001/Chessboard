ae8aaebe272f26e4b856e1718c9eeab57805d6f1
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 15:19:52 +0530

    Added landing page

--- frontend/src/screens/Landing.jsx
+++ frontend/src/screens/Landing.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom"
+
 function Landing(){
+
+    const navigate = useNavigate();
 
     return (
         <div className="flex justify-center">
@@ -14,7 +18,7 @@ function Landing(){
                             </h1>
                         </div>
                         <div className="mt-8 flex justify-center">
-                            <button className="px-8 py-4 text-2xl bg-green-500 hover:bg-green-700 text-white font-bold rounded">
+                            <button onClick={() => navigate("/game")} className="px-8 py-4 text-2xl bg-green-500 hover:bg-green-700 text-white font-bold rounded">
                                 Play Now
                             </button>
                         </div>
