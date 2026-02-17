b7469e00c89134139d15c25a5cb6f52d92260cf0
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 15:23:41 +0530

    Added game

--- frontend/src/screens/Game.jsx
+++ frontend/src/screens/Game.jsx
@@ -1,8 +1,13 @@
 function Game(){
 
     return (
-        <div>
-            This is a game page
+        <div  className="flex justify-center">
+            <div className="pt-8 max-w-5xl">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                    <Chessboard />
+                </div>
+            </div>
+
         </div>
     )
 };
