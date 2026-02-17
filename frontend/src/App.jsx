bd0cab8577e9666f509616da0f8b29aac015a8d4
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 14:37:22 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Landing } from './screens/Landing';
+import { Game } from './screens/Game';
 
 function App() {
   
---
+++ frontend/src/screens/Game.jsx
@@ -0,0 +1,12 @@
+function Game(){
+
+    return (
+        <div>
+            This is a game page
+        </div>
+    )
+};
+
+export {
+    Game
+}
