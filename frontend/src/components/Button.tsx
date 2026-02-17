ab56cf197a2cf3e7400d354d7921fc9b1f1c2090
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 16:56:54 +0530

    Added button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -4,7 +4,7 @@ function Button({ children, onClick}){
 
     const navigate = useNavigate();
     return(
-        <button onClick={onClick} className="px-8 py-4 text-2xl bg-green-500 hover:bg-green-700 text-white font-bold rounded">
+        <button onClick={onClick} className="px-8 py-4 text-2xl bg-green-500 hover:bg-green-700 text-white font-bold rounded hover:cursor-pointer">
             {children}
         </button>
     )
