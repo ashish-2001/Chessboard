7b7f7aeedacb11c6cabca299ee9fe148922451b7
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 15:07:54 +0530

    Added Landing page

--- frontend/src/screens/Landing.jsx
+++ frontend/src/screens/Landing.jsx
@@ -2,7 +2,23 @@ function Landing(){
 
     return (
         <div>
-            Landing Page
+            <div className="mt-2">
+                <div className="grid grid-cols-1 gap-4">
+                    <div >
+                        <img src="/Chessboard.jpeg" className="max-w-96 "/>
+                    </div>
+                    <div className="flex justify-center">
+                        <h1 className="text-4xl font-bold">
+                            Play chess online on the #3 site.
+                        </h1>
+                        <div className="mt-4">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Play Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
