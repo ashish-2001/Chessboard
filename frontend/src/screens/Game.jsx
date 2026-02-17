70f1fb3480a3858956e07d8a32278832dff510b0
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-17 15:32:24 +0530

    Added game

--- frontend/src/screens/Game.jsx
+++ frontend/src/screens/Game.jsx
@@ -4,9 +4,14 @@ function Game(){
 
     return (
         <div  className="flex justify-center">
-            <div className="pt-8 max-w-5xl">
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                    <Chessboard />
+            <div className="pt-8 max-w-5xl w-full">
+                <div className="grid grid-cols-6 gap-4 w-full">
+                    <div className="grid-cols-span-4 bg-red-200 w-full">
+                        <Chessboard />
+                    </div>
+                    <div className="col-span-2 bg-green-200 w-full">
+
+                    </div>
                 </div>
             </div>
 
