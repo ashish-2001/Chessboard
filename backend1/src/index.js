5ecb71d5a06a13abfae3abd998483c01e9cc306d
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-18 14:06:26 +0530

    Added index

--- backend1/src/index.js
+++ backend1/src/index.js
@@ -1,9 +1,9 @@
 import { WebSocketServer } from "ws";
 import { GameManager } from "./GameManager.js";
 
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
-const wss = new WebSocketServer({ PORT });
+const wss = new WebSocketServer({ port });
 const gameManager = new GameManager();
 
 wss.on("connection", function connection(ws){
